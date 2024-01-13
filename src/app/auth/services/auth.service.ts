2619ad6ed8a6f6036a703322561666125e6b6542
Diegucho7 <87243460+Diegucho7@users.noreply.github.com>
2024-01-13 16:40:29 -0500

    setAuthentication

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -25,6 +25,13 @@ export class AuthService {
 
   constructor() { }
 
+  private setAuthentication(user:User, token:string ):boolean{
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token', token);
+    return true;
+  }
+
   login (email:string, password:string): Observable<boolean>{
 
     const url = `${this.baseUrl}/auth/login`;
@@ -32,14 +39,8 @@ export class AuthService {
 
     return this.http.post<LoginResponse>(url,body)
     .pipe(
-      tap( ({user, token}) => {
-        
-          this._currentUser.set(user);
-          this._authStatus.set(AuthStatus.authenticated);
-          localStorage.setItem('token', token)
-          console.log({user,token});
-
-        }),
+      map(({token, user})=> this.setAuthentication(user,token)),
+      
         map( ()=>true),
 
         // TODO:Errores
@@ -61,14 +62,8 @@ export class AuthService {
        set('Authorization',`Bearer ${token}`);
 
        return this.http.get<CheckTokenResponse>(url, {headers})
-       .pipe(
-        map(({token, user})=> {
-          this._currentUser.set(user);
-          this._authStatus.set(AuthStatus.authenticated);
-          localStorage.setItem('token', token)
-         
-          return true;
-        }),
+       .pipe( 
+        map(({token, user})=> this.setAuthentication(user,token)),
         catchError( () => {
           this._authStatus.set(AuthStatus.notAuthenticated)
          return of(false);
