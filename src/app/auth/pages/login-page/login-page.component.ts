cd1eb8000dd15a22e3cf7c158839d13f4f239bda
Diegucho7 <87243460+Diegucho7@users.noreply.github.com>
2024-01-12 20:45:43 -0500

    swal

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
-import { error } from 'node:console';
+import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
 
 
 @Component({
@@ -11,19 +12,20 @@ import Swal from 'sweetalert2';
 })
 export class LoginPageComponent {
 
-  private fb = inject (FormBuilder);
+  private fb          = inject (FormBuilder);
   private AuthService = inject(AuthService);
+  private router      = inject ( Router )
 
   public myForm: FormGroup = this.fb.group({
     email:['dvelarde140@gmail.com', [Validators.required, Validators.email]],
-    password:['1234567', [Validators.required, Validators.minLength(6)]]
+    password:['123456', [Validators.required, Validators.minLength(6)]]
   });
 
   login(){
     const {email,password} = this.myForm.value;
     this.AuthService.login(email,password).
     subscribe({
-      next: () => console.log('Todo bien'),
+      next: () => this.router.navigateByUrl('/dasboard'),
       error: (message) => {
         Swal.fire('Error',message,'error')
       }
